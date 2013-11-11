54b2fed0e56f635481fbbcf32ca4144456ae4870
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2013-11-11 11:03:48 +0800

    Yay.

--- chengyu_scraper.rb
+++ chengyu_scraper.rb
@@ -2,12 +2,15 @@
 require "nokogiri"
 require "open-uri"
 
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 class ChengyuScraper
 	def initialize
 	end
 
 	def chengyu_url(chengyu)
-	  URI.escape("http://en.wiktionary.org/wiki/#{chengyu}")
+	  URI.escape("http://en.wiktionary.org/wiki/#{chengyu}".strip)
 	end
 
 	def run!
@@ -20,7 +23,7 @@ class ChengyuScraper
 		doc                 = Nokogiri::HTML(open(chengyu_url(chengyu)))
 		pinyin              = doc.at('i:contains("Pinyin")').next_element.text.strip
 		english_explanation = doc.at('ol').children.first.text.strip
-		[chengyu, pinyin, english_explanation]
+		puts [chengyu, pinyin, english_explanation]
 	end
 end
 
